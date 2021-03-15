1b9c4f6b56294d46e2bcafe73b23e8b9a777622d
francescogrossi92 <francesco.grossi@alten.it>
2021-03-15 17:36:27 +0100

    iniziamo il progetto

---
+++ java/grossi/chucknorris/spring5jokersapp/Spring5JokersAppV2Application.java
@@ -0,0 +1,13 @@
+package grossi.chucknorris.spring5jokersapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5JokersAppV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5JokersAppV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/grossi/chucknorris/spring5jokersapp/Spring5JokersAppV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package grossi.chucknorris.spring5jokersapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5JokersAppV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
